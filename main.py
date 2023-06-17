87604e8f52c9e1224233039138dbcd553d1dc4a8
ZooMix <salnikovsanya2002@gmail.com>
2023-06-17 13:17:46 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+# a = int(input('Введите число: '))
+# print(a + 5)
+
+# Строки
+# a = 'Привет'
+# print(type(a))
+#
+# b = 34
+# print(type(b))
+#
+# c = 4.56
+# print(type(c))
+#
+# d = True
+# print(type(d))
+
+# a = 5
+# b = 2
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print(a ** b)
+
+# print(a < b)
+# print(a > b)
+# print(a <= b)
+# print(a >= b)
+# print(a == b)
+# print(a != b)
+
+
+# a = int(input('Введите число: '))
+# if a > 0:
+#     if a != 10:
+#         print(f'Число {a} положительное и не равно 10!')
+# elif a == 0:
+#     print('Ноль')
+# else:
+#     print(f'Число {a} отрицательное!')
+
