63eeb21dc732caea6f4135f79502b776f10e863d
ZooMix <salnikovsanya2002@gmail.com>
2023-06-19 22:50:57 +0500

    Cправочник

--- find.py
+++ find.py
@@ -1,14 +1,12 @@
 def find_person(surname: str):
     with open('test.txt', 'r', encoding='utf-8') as file:
-        line_list = file.read().splitlines()
-        found = False
-        for ind in range(len(line_list)):
-            if line_list[ind] == surname.title():
-                found = True
-                print(line_list[ind])
-                print(line_list[ind + 1])
-                print(line_list[ind + 2])
-                print(line_list[ind + 3])
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            if line[:-1] == surname:
+                print(line.strip())
+                print(file.readline().strip())
+                print(file.readline().strip())
+                print(file.readline().strip())
                 print()
-        if not found:
-            print('Контакт не найден!')
